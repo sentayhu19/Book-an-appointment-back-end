bbec286220cfce8d9fcfc80af27e5c4692df70fb
Achilles-Dev <solohag92@gmail.com>
2022-08-12 10:29:58 +0000

    Add api sessions contoller

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -7,8 +7,9 @@ class RegistrationsController < Devise::RegistrationsController
     user = User.new(sign_up_params)
     if user.save!
       render json: user, status: :ok
+      redirect_to root_path
     else
-      render json: { errors: motor.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
---
+++ app/controllers/api/sessions_constroller.rb
@@ -0,0 +1,11 @@
+class SessionsController < Devise::SessionsController
+  def create
+    user = User.find_by(name: params[:user][:name])
+    if user.present?
+      render json: user, status: :ok
+      redirect_to root_path
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
