20fe7169625218fefafba70fdde629d78176a44b
Achilles-Dev <solohag92@gmail.com>
2022-08-12 10:06:03 +0000

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  respond_to  :json
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: :name)
+  end
 end
