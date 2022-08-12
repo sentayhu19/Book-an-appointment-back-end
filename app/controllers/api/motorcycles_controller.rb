7e110366bec57b500e195e9a488355053afff2f9
Achilles-Dev <solohag92@gmail.com>
2022-08-12 10:05:27 +0000

    Add motorcycles controller methods

---
+++ app/controllers/api/motorcycles_controller.rb
@@ -0,0 +1,47 @@
+class Api::MotorcyclesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def index
+    @motors = Motorcycle.all
+    render json: @motors
+  end
+
+  def new
+    @motor = Motorcycle.new
+  end
+
+  def create
+    motor = Motorcycle.new(motor_params.merge(user: @user))
+    if @motor.save
+      render json: motor, status: :ok
+    else
+      render json: { errors: motor.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @motor = Motorcycle.find(params[:id])
+    render json: @motor
+  end
+
+  def update
+    motor = Motorcycle.find(params[:id])
+    motor.update(motor_params)
+  end
+
+  def destroy; end
+  motor = Motorcycle.find(params[:id])
+  motor.destroy
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def motor_params
+    params.require('motors').permit(:model, :deposit, :total_payable, :finance_fee, :purchase_fee, :duration_months,
+                                    :apr_rep)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_231834) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_231507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,17 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_231834) do
     t.decimal "apr_rep"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "motorcycle_id", null: false
-    t.string "city"
-    t.string "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["motorcycle_id"], name: "index_reservations_on_motorcycle_id"
-    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,6 +40,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_231834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reservations", "motorcycles"
-  add_foreign_key "reservations", "users"
 end
