97592eecf7b1f56de0b09e660b2654c609157bc4
debas-31 <debas31@gmail.com>
2022-08-16 15:26:24 +0300

    Add test for update and delete method of motorcycle controller

--- spec/requests/api/motorcycles_spec.rb
+++ spec/requests/api/motorcycles_spec.rb
@@ -62,5 +62,34 @@ RSpec.describe 'Motorcycles', type: :request do
         run_test!
       end
     end
+    put('update motorcycles') do
+        response(200, 'successful') do
+          let(:id) { '123' }
+  
+          after do |example|
+            example.metadata[:response][:content] = {
+              'application/json' => {
+                example: JSON.parse(response.body, symbolize_names: true)
+              }
+            }
+          end
+          run_test!
+        end
+      end
+  
+      delete('delete motorcycle') do
+        response(200, 'successful') do
+          let(:id) { '123' }
+  
+          after do |example|
+            example.metadata[:response][:content] = {
+              'application/json' => {
+                example: JSON.parse(response.body, symbolize_names: true)
+              }
+            }
+          end
+          run_test!
+        end
+      end
   end
 end
