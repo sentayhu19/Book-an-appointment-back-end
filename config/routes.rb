6b18b9ce9e0865cf4c495cf35329051f314a58df
debas-31 <debas31@gmail.com>
2022-08-16 13:07:17 +0300

    Generate rswag file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   scope :api do
     devise_for :users, controllers: { sessions: :'api/sessions', registrations: :'api/registrations'}
   end
